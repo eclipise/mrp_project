f25dc214750e7798a50a4818aa33f955e9422c3b
Garrett Macomber <96957645+eclipise@users.noreply.github.com>
2023-05-28 17:38:26 -0400

    partial refactor of Arduino code

--- src/jetson/arduino_controller.py
+++ src/jetson/arduino_controller.py
@@ -5,19 +5,28 @@ class ArduinoController:
         # establishes a serial connection to the Arduino
         self.arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 
-    def send_movement(self, values: tuple[int, int, int]) -> str:
+    # Send a movement command and returns any messages sent back
+    def send_movement(self, values: tuple[int, int, int]) -> list:
         # "speed turn duration"
-        message = f"{values[0]} {values[1]} {values[2]}"
+        message = f"{values[0]} {values[1]} {values[2]}\n"
         
         # sends the message to the Arduino as a byte string
         self.arduino.write(message.encode())
 
-        # waits for the Arduino to send a response
-        while self.arduino.in_waiting == 0:
-            pass
+        # Stores any messages the Arduino sends back, demarcated by newlines
+        response = list()
 
-        # reads a string from the Arduino
-        response = self.arduino.readline().decode()
+        # receives the Arduino's response, in an unknown number of parts, until it sends a line
+        # beginning with "END"
+        while True:
+            # if the connection is not idle, append received line to response
+            if self.arduino.in_waiting > 0:
+                line = self.arduino.readline().decode()
+                response.append(line)
+                
+                # checks if the first three characters of the response are "END"
+                if(line[:2] == "END"):
+                    break
 
         return response
     
