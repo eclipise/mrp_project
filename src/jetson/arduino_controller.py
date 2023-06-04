d76cc23ed9612e91409eb340f623fb7d4e6f08d9
Garrett Macomber <96957645+eclipise@users.noreply.github.com>
2023-06-04 16:39:49 -0400

    refactored jetson code

--- src/jetson/arduino_controller.py
+++ src/jetson/arduino_controller.py
@@ -2,35 +2,47 @@ import serial
 
 class ArduinoController:
     def __init__(self) -> None:
-        # establishes a serial connection to the Arduino, falls back to debug if this fails
-        try:
-            self.arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-        except serial.serialutil.SerialException as e:
-            print("Arduino not found:", e)
-            self.arduino = None
+        self.connected = False
+        
+        # Attempts to connect to the arduino
+        self.connect_arduino
 
-    # Send a movement command and returns any messages sent back
+    # Sends a movement command and returns any messages sent back
     def send_movement(self, values: tuple[int, int, int]) -> list:
+        # Errors if the Arduino is not connected
+        if not self.arduino_connected:
+            return list("Error: Arduino not connected", "END")
+        
         # "speed turn duration"
         message = f"{values[0]} {values[1]} {values[2]}\n"
         
-        # sends the message to the Arduino as a byte string
+        # Sends the message to the Arduino as a byte string
         self.arduino.write(message.encode())
 
         # Stores any messages the Arduino sends back, demarcated by newlines
         response = list()
 
-        # receives the Arduino's response, in an unknown number of parts, until it sends a line
-        # beginning with "END"
+        # Receives the Arduino's response, in an unknown number of parts, until it sends a line
+        # beginning with "END".
         while True:
-            # if the connection is not idle, append received line to response
+            # If the connection is not idle, append received line to response
             if self.arduino.in_waiting > 0:
                 line = self.arduino.readline().decode()
                 response.append(line)
                 
-                # checks if the first three characters of the response are "END"
+                # Breaks if the first three characters of the response are "END"
                 if(line[:2] == "END"):
                     break
 
         return response
+    
+    # Establishes a serial connection to the Arduino, prints an error on fail
+    def connect_arduino(self):
+        try:
+            self.arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+            self.connected = True
+        except serial.serialutil.SerialException as e:
+            print("Arduino not found:", e)
+            self.arduino = None
+            self.connected = False
     
--- src/jetson/server.py
+++ src/jetson/server.py
@@ -3,7 +3,6 @@ from arduino_controller import ArduinoController
 
 app = Flask(__name__)
 arduino_controller = ArduinoController()
-arduino_connection = arduino_controller.arduino != None
 
 @app.get("/")
 def base_handler():
@@ -15,15 +14,15 @@ def move_handler():
 
     values = validate_movement(message)
 
-    # if there are no errors, remove that from the tuple; else send the errors
+    # If there are no errors, remove that from the tuple; else send the errors
     if len(values[3]) == 0:
         values = (values[0], values[1], values[2])
     else:
-        return {"errors": values[3]}, 400
+        return {"errors": values[3]}, 400 # 400: bad request
     
-    # communicates with the Arduino if one was found, falls back on debug printing otherwise
-    if arduino_connection:
-        # sends the movement command to the Arduino and gets its response
+    # Communicates with the Arduino if one was found, falls back on debug printing otherwise
+    if arduino_controller.connected:
+        # Sends the movement command to the Arduino and gets its response
         arduino_response = arduino_controller.send_movement(values)
     else:
         print("Server received:", values)
@@ -34,7 +33,7 @@ def move_handler():
                             "Status: Robot stopped",
                             "END: Success"]
 
-    # sends the Arduino's response to the client
+    # Sends the Arduino's response to the client
     return {"arduino response": arduino_response}, 200
 
 @app.get("/status")
@@ -46,17 +45,17 @@ def status_handler():
     return status
 
 def validate_movement(message: dict) -> tuple[int, int, int, list]:
-    # checks if the required keys are present
+    # Checks if the required keys are present
     speed_present = "speed" in message.keys()
     turn_present = "turn" in message.keys()
     duration_present = "duration" in message.keys()
     
     # #######################################################################
     # If a key is not present, appends the relevant error to return message.
-    # If a key is present, checks if its value in the valid range and 
+    # If a key is present, checks if its value is in the valid range and 
     # appends the relevant error to the return message if it is not.
 
-    # remains empty if there are no errors
+    # Remains empty if there are no errors
     errors = list()
 
     if speed_present:
@@ -90,12 +89,12 @@ def validate_movement(message: dict) -> tuple[int, int, int, list]:
 
     # #######################################################################
 
-    # returns values as ints and either an error string or an empty string on success
+    # Returns values as ints and either an error string or an empty string on success
     return (speed, turn, duration, errors)
 
 
 if __name__ == "__main__":
-    if not arduino_connection:
+    if not arduino_controller.connected:
         print("Running server without Arduino connection.")
     
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=False, host="0.0.0.0")
