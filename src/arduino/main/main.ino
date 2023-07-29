d7e4f92ce56b7356e2865f88b7c98670d17bb38e
Garrett <96957645+eclipise@users.noreply.github.com>
2023-07-29 18:18:46 -0400

    updated Arduino code

--- src/arduino/main/main.ino
+++ src/arduino/main/main.ino
@@ -1,35 +1,28 @@
-#include <ros.h>
-#include <geometry_msgs/Twist.h>
-#include <std_msgs/String.h>
-#include <std_msgs/Int16.h>
 #include <SharpIR.h>
 
-// Base component necessary to make this program a ROS node
-ros::NodeHandle nh;
+/* ----------------------------- Program config ----------------------------- */
 
-/* ---------------------------- Program constants --------------------------- */
+const int BAUD_RATE = 57600;
 
 // Distance in cm at which an IR sensor is considered blocked when moving
 // forward or backwards
-const int LINEAR_CLEAR_THRESHOLD = -1;
+int LINEAR_CLEAR_THRESHOLD = 25;
 // Distance in cm at which an IR sensor is considered blocked when turning
-const int TURN_CLEAR_THRESHOLD = -1;
+int TURN_CLEAR_THRESHOLD = 20;
 
 // Values below PWM_MIN will be treated as 0
-const int PWM_MIN = 24; // one less than 10% power
+int PWM_MIN = 24; // 10% power
 // Values above PWM_MAX will be reduced to PWM_MAX
-const int PWM_MAX = 50; // 20% power
+int PWM_MAX = 50; // 20% power
 // PWM value used when turning in place (+/- for each side, based on direction)
-const int PWM_TURN = 75; // 30% power
+int PWM_TURN = 75; // 30% power
 
 // Time in milliseconds to continue the last command before stopping, in absence
 // of a new command
-const unsigned COMMAND_TIMEOUT = 200;
+unsigned COMMAND_TIMEOUT = 200;
 // Time in milliseconds between checking if the IR sensors are blocked. Should
 // always be greater than 30 ms to prevent an internal delay in SharpIR.
 const unsigned IR_POLL = 200;
-// Rate in ms at which encoder ticks are published
-const unsigned ENC_POLL = 200;
 
 // Min/max value of the tick count for under- and overflow
 const unsigned ENC_BOUND = 32767;
@@ -68,114 +61,89 @@ SharpIR IR_RR(model, A3); // Rear right
 SharpIR IR_RL(model, A4); // Rear left
 SharpIR IR_FC(model, A5); // Front center
 
-/* --------------------- ROS publishers and message data -------------------- */
-
-std_msgs::String msg;
-ros::Publisher chatter("chatter", &msg);
-
-std_msgs::Int16 FL_ticks;
-ros::Publisher FL_Pub("FL_ticks", &FL_ticks);
-
-std_msgs::Int16 FR_ticks;
-ros::Publisher FR_Pub("FR_ticks", &FR_ticks);
-
-std_msgs::Int16 RR_ticks;
-ros::Publisher RR_Pub("RR_ticks", &RR_ticks);
-
-std_msgs::Int16 RL_ticks;
-ros::Publisher RL_Pub("RL_ticks", &RL_ticks);
-
 /* ------------------------------ Program data ------------------------------ */
 
 // These values are from -255 to 255, indicating required speed and direction
 int pwmLeftReq = 0;
 int pwmRightReq = 0;
 
-// Tracks whether the wheels on each side have been told to move forward or
-// backwards, which is only used for the encoders. Updated when PWM is set,
-// remains unchanged when robot is ordered to stop.
-bool left_moving_forward = true;
-bool right_moving_forward = true;
+// Tracks whether the motors have been told to move forward or backwards, which
+// is only used for the encoders. Updated when PWM is set, remains unchanged
+// when robot is ordered to stop.
+bool fl_moving_forward = true;
+bool fr_moving_forward = true;
+bool rr_moving_forward = true;
+bool rl_moving_forward = true;
 
 // Timestamp in milliseconds of the last command
 unsigned long lastCmdTime = 0;
 // Timestamp in milliseconds of the last IR check
 unsigned long lastIRTime = 0;
 // Timestamp in milliseconds of the last publish of encoder data
-unsigned long lastEncTime = 0; 
+unsigned long lastEncTime = 0;
 
 // IR sensor data
-int fr_dist, fl_dist, rl_dist, rr_dist, fc_dist;
+int fl_dist, fr_dist, rr_dist, rl_dist, fc_dist;
+
+volatile long fl_ticks, fr_ticks, rr_ticks, rl_ticks;
 
 /* -------------------------------- Encoders -------------------------------- */
 
-// Since the encoders only register ticks, not direction, these tick functions
-// fudge the direction by looking at whether the robot was last told to move
-// forward or backwards. This will usually work even for drift after the robot
-// has been told to stop, but won't catch movement in a different direction than
-// expected.
-
-// When the tick count approaches an over- or underflow, it is re-centered to 0.
-// This prevents continuous wrapping if, for example, the robot is rocking in
-// place and the tick count is at an extreme. That would be problematic because
-// the downstream velocity calculation ignores ticks from intervals when it
-// wraps and would never yield a value.
-
 void FL_tick() {
-    if (abs(FL_ticks.data) == ENC_BOUND) {
-        FL_ticks.data = 0;
+    if (abs(fl_ticks) == ENC_BOUND) {
+        fl_ticks = 0;
     }
-    
-    if (left_moving_forward) {
-        FL_ticks.data++;
+
+    if (fl_moving_forward) {
+        fl_ticks++;
     } else {
-        FL_ticks.data--;
+        fl_ticks--;
     }
 }
 
 void FR_tick() {
-    if (abs(FR_ticks.data) == ENC_BOUND) {
-        FR_ticks.data = 0;
+    if (abs(fr_ticks) == ENC_BOUND) {
+        fr_ticks = 0;
     }
 
-    if (left_moving_forward) {
-        FR_ticks.data++;
+    if (fr_moving_forward) {
+        fr_ticks++;
     } else {
-        FR_ticks.data--;
+        fr_ticks--;
     }
 }
 
 void RR_tick() {
-    if (abs(RR_ticks.data) == ENC_BOUND) {
-        RR_ticks.data = 0;
+    if (abs(rr_ticks) == ENC_BOUND) {
+        rr_ticks = 0;
     }
 
-    if (right_moving_forward) {
-        RR_ticks.data++;
+    if (rr_moving_forward) {
+        rr_ticks++;
     } else {
-        RR_ticks.data--;
+        rr_ticks--;
     }
 }
 
 void RL_tick() {
-    if (abs(RL_ticks.data) == ENC_BOUND) {
-        RL_ticks.data = 0;
+    if (abs(rl_ticks) == ENC_BOUND) {
+        rl_ticks = 0;
     }
 
-    if (right_moving_forward) {
-        RL_ticks.data++;
+    if (rl_moving_forward) {
+        rl_ticks++;
     } else {
-        RL_ticks.data--;
+        rl_ticks--;
     }
 }
 
 /* ------------------------------- IR Sensors ------------------------------- */
 
-void updateDistance() {
-    fr_dist = IR_FR.getDistance();
+void updateIR() {
     fl_dist = IR_FL.getDistance();
-    rl_dist = IR_RL.getDistance();
+    fr_dist = IR_FR.getDistance();
     rr_dist = IR_RR.getDistance();
+    rl_dist = IR_RL.getDistance();
     fc_dist = IR_FC.getDistance();
 }
 
@@ -200,17 +168,14 @@ bool areaClear() {
 
 bool checkClear() {
     // If not turning
-    if (pwmLeftReq == pwmRightReq) {
-        if (pwmLeftReq > 0 && !frontClear()) {
+    if (fl_moving_forward == fr_moving_forward) {
+        if (fl_moving_forward && !frontClear()) {
             // If moving forward and front is not clear
             return false;
-        } else if (pwmLeftReq < 0 && !backClear()) {
+        } else if (!fl_moving_forward && !backClear()) {
             // If moving backwards and rear is not clear
             return false;
         }
-
-        // Else PWM is 0 and the robot isn't moving
-        return true;
     } else if (!areaClear()) {
         // If turning and area is not clear
         return false;
@@ -219,127 +184,182 @@ bool checkClear() {
     return true;
 }
 
-/* ------------------------------ Motor Control ----------------------------- */
+/* --------------------------------- Motors --------------------------------- */
 
-// Updates the required PWM values every time a new ROS command comes in, but
-// does not update the values sent to the motors.
-void calc_pwm(const geometry_msgs::Twist &cmdVel) {
-    msg.data = "calculating pwm";
-    chatter.publish(&msg);
+// int constrain_pwm(int pwm) {
 
-    lastCmdTime = millis();
+//     // Constrain left
+//     if (abs(pwmLeftReq) < PWM_MIN) {
+//         pwmLeftReq = 0;
+//     } else if (abs(pwmLeftReq) > PWM_MAX) {
+//         // Sets the PWM to the max value, but with the same sign
+//         pwmLeftReq = PWM_MAX * ((pwmLeftReq > 0) - (pwmLeftReq < 0));
+//     }
 
-    // For now, this interprets incoming cmdVel messages with m/s and rad/s
-    // values as percents of max PWM. Full forward is 1.00 m/s, reverse is -1.00
-    // m/s, full left turn is 1.00 rad/s, full right turn is -1.00 rad/s, etc.
-    // Uses linear.x and angular.z.
+//     // Constrain right
+//     if (abs(pwmRightReq) < PWM_MIN) {
+//         pwmRightReq = 0;
+//     } else if (abs(pwmRightReq) > PWM_MAX) {
+//         // Sets the PWM to the max value, but with the same sign
+//         pwmRightReq = PWM_MAX * ((pwmRightReq > 0) - (pwmRightReq < 0));
+//     }
+// }
 
-    pwmLeftReq = cmdVel.linear.x * 255;
-    pwmRightReq = cmdVel.linear.x * 255;
-
-    // If commanded to turn
-    if (cmdVel.angular.z != 0) {
-        if (cmdVel.angular.z > 0) {
-            // Left turn
-            pwmLeftReq = -PWM_TURN;
-            pwmRightReq = PWM_TURN;
-        } else {
-            // Right turn
-            pwmLeftReq = PWM_TURN;
-            pwmRightReq = -PWM_TURN;
-        }
-    }
-
-    /* ------------- Constrains PWM values using PWM_MIN and PWM_MAX ------------ */
-    // Constrain left
-    if (abs(pwmLeftReq) < PWM_MIN) {
-        pwmLeftReq = 0;
-    } else if (abs(pwmLeftReq) > PWM_MAX) {
-        // Sets the PWM to the max value, but with the same sign
-        pwmLeftReq = PWM_MAX * ((pwmLeftReq > 0) - (pwmLeftReq < 0));
-    }
-
-    // Constrain right
-    if (abs(pwmRightReq) < PWM_MIN) {
-        pwmRightReq = 0;
-    } else if (abs(pwmRightReq) > PWM_MAX) {
-        // Sets the PWM to the max value, but with the same sign
-        pwmRightReq = PWM_MAX * ((pwmRightReq > 0) - (pwmRightReq < 0));
-    }
-    /* -------------------------------------------------------------------------- */
-}
-
-void set_pwm() {
-    msg.data = "setting pwm";
-    chatter.publish(&msg);
-    
-    /* -------------------- Sets the direction of the motors -------------------- */
-    if (pwmLeftReq > 0) {
-        // Left forward
+// TODO: check directions and PWM pins
+void set_pwm(int fl_pwm, int fr_pwm, int rr_pwm, int rl_pwm) {
+    // Front left
+    if (fl_pwm > 0) {
+        // Forward
         digitalWrite(L_INT1, 1);
         digitalWrite(L_INT2, 0);
+
+        fl_moving_forward = true;
+    } else if (fl_pwm < 0) {
+        // Backward
+        digitalWrite(L_INT1, 0);
+        digitalWrite(L_INT2, 1);
+
+        fl_moving_forward = false;
+    } else {
+        // Stop
+        digitalWrite(L_INT1, 0);
+        digitalWrite(L_INT2, 0);
+
+        // fl_moving_forward remains at its last value to account for drift
+    }
+
+    analogWrite(L_ENA, fl_pwm);
+
+    // Front right
+    if (fr_pwm > 0) {
+        // Forward
         digitalWrite(L_INT3, 1);
         digitalWrite(L_INT4, 0);
 
-        left_moving_forward = true;
-    } else if (pwmLeftReq < 0) {
-        // Left reverse
-        digitalWrite(L_INT1, 0);
-        digitalWrite(L_INT2, 1);
-        digitalWrite(L_INT3, 0);
-        digitalWrite(L_INT4, 1);
-
-        left_moving_forward = false;
-    } else {
-        // Left stop
-        digitalWrite(L_INT1, 0);
-        digitalWrite(L_INT2, 0);
-        digitalWrite(L_INT3, 0);
-        digitalWrite(L_INT4, 0);
-
-        // left_moving_forward remains at its last value to account for drift
-    }
-
-    if (pwmRightReq > 0) {
-        // Right forward
-        digitalWrite(R_INT1, 1);
-        digitalWrite(R_INT2, 0);
-        digitalWrite(R_INT3, 0);
-        digitalWrite(R_INT4, 1);
-
-        right_moving_forward = true;
-    } else if (pwmRightReq < 0) {
-        // Right reverse
-        digitalWrite(R_INT1, 0);
-        digitalWrite(R_INT2, 1);
+        fr_moving_forward = true;
+    } else if (fr_pwm < 0) {
+        // Backward
         digitalWrite(R_INT3, 1);
         digitalWrite(R_INT4, 0);
 
-        right_moving_forward = false;
+        fr_moving_forward = false;
     } else {
-        // Right stop
-        digitalWrite(R_INT1, 0);
-        digitalWrite(R_INT2, 0);
+        // Stop
+        digitalWrite(L_INT3, 0);
+        digitalWrite(L_INT4, 0);
+    }
+
+    analogWrite(L_ENB, fr_pwm);
+
+    // Rear right
+    if (rr_pwm > 0) {
+        // Forward
+        digitalWrite(R_INT3, 0);
+        digitalWrite(R_INT4, 1);
+
+        rr_moving_forward = true;
+    } else if (rr_pwm < 0) {
+        // Backward
+        digitalWrite(L_INT3, 0);
+        digitalWrite(L_INT4, 1);
+
+        rr_moving_forward = false;
+    } else {
+        // Stop
         digitalWrite(R_INT3, 0);
         digitalWrite(R_INT4, 0);
-
-        // right_moving_forward remains at its last value to account for drift
     }
-    /* -------------------------------------------------------------------------- */
 
-    // Writes the PWM values (which should already be constrained)
-    analogWrite(L_ENA, abs(pwmLeftReq));
-    analogWrite(L_ENB, abs(pwmLeftReq));
-    analogWrite(R_ENA, abs(pwmRightReq));
-    analogWrite(R_ENB, abs(pwmRightReq));
+    analogWrite(R_ENA, rr_pwm);
+
+    // Rear left
+    if (rl_pwm > 0) {
+        // Forward
+        digitalWrite(R_INT1, 1);
+        digitalWrite(R_INT2, 0);
+
+        rl_moving_forward = true;
+    } else if (rl_pwm < 0) {
+        // Backward
+        digitalWrite(R_INT1, 0);
+        digitalWrite(R_INT2, 1);
+
+        rl_moving_forward = false;
+    } else {
+        // Stop
+        digitalWrite(R_INT1, 0);
+        digitalWrite(R_INT2, 0);
+    }
+
+    analogWrite(R_ENB, rl_pwm);
 }
 
-/* -------------------------------------------------------------------------- */
+/* ----------------------------- Program control ---------------------------- */
 
-// Sets a ROS subscriber to handle velocity commands with the calc_pwm function
-ros::Subscriber<geometry_msgs::Twist> CmdVel_Sub("cmd_vel", &calc_pwm);
+void run_command(char cmd_sel, int arg1, int arg2, int arg3, int arg4) {
+    switch (cmd_sel) {
+    // Encoder read
+    case 'e':
+        Serial.print(fl_ticks);
+        Serial.print(" ");
+        Serial.print(fr_ticks);
+        Serial.print(" ");
+        Serial.print(rr_ticks);
+        Serial.print(" ");
+        Serial.println(rl_ticks);
+        break;
 
-// Runs once on Arduino startup
+    // Set motor pwm
+    case 'm':
+        lastCmdTime = millis();
+        set_pwm(arg1, arg2, arg3, arg4);
+        break;
+
+    // IR read
+    case 'i':
+        updateIR();
+
+        Serial.print(fl_dist);
+        Serial.print(" ");
+        Serial.print(fc_dist);
+        Serial.print(" ");
+        Serial.print(fr_dist);
+        Serial.print(" ");
+        Serial.print(rl_dist);
+        Serial.print(" ");
+        Serial.println(rr_dist);
+        break;
+
+    // IR check
+    case 'c':
+        updateIR();
+        Serial.println(checkClear());
+        break;
+
+    // IR threshold config
+    case 't':
+        LINEAR_CLEAR_THRESHOLD = arg1;
+        TURN_CLEAR_THRESHOLD = arg2;
+        break;
+
+    // Command timeout config
+    case 'o':
+        COMMAND_TIMEOUT = arg1;
+        break;
+
+    // Max speed config
+    case 's': 
+        PWM_MIN = arg1;
+        PWM_MAX = arg2;
+        PWM_TURN = arg3;
+        break;
+
+    default:
+        Serial.println("Invalid command.");
+        break;
+    }
+}
+
 void setup() {
     // Sets all motor control pins to output mode
     pinMode(L_ENA, OUTPUT);
@@ -354,6 +374,12 @@ void setup() {
     pinMode(R_INT2, OUTPUT);
     pinMode(R_INT3, OUTPUT);
     pinMode(R_INT4, OUTPUT);
+
+    // Sets the encoder pins to input mode
+    pinMode(FL_ENC, INPUT);
+    pinMode(FR_ENC, INPUT);
+    pinMode(RR_ENC, INPUT);
+    pinMode(RL_ENC, INPUT);
 
     // Attaches interrupts to encoder pins, calls given functions on rising edge
     attachInterrupt(digitalPinToInterrupt(FL_ENC), FL_tick, RISING);
@@ -377,60 +403,36 @@ void setup() {
     analogWrite(R_ENA, 0);
     analogWrite(R_ENB, 0);
 
-    // ROS setup
-    nh.initNode();
-    nh.advertise(chatter);
-    nh.advertise(FL_Pub);
-    nh.advertise(FR_Pub);
-    nh.advertise(RR_Pub);
-    nh.advertise(RL_Pub);
-    nh.subscribe(CmdVel_Sub);
+    Serial.begin(BAUD_RATE);
 }
 
-// Main loop, runs repeatedly while Arduino is on
 void loop() {
-    // Handles incoming ROS messages (which calls calc_pwm per the CmdVel_Sub
-    // subscriber)
-    nh.spinOnce();
+    if (Serial.available() > 0) {
+        String command = Serial.readStringUntil('\n');
 
-    unsigned long currentTime = millis();
+        char cmd_sel = '\0';
+        int arg1 = 0, arg2 = 0, arg3 = 0, arg4 = 0;
 
-    // Stops the robot if it has been more than COMMAND_TIMEOUT ms since the
-    // last instruction
-    if (currentTime - lastCmdTime > COMMAND_TIMEOUT) {
-        msg.data = "timeout";
-        chatter.publish(&msg);
+        int num_parsed = sscanf(command.c_str(), "%c %d %d %d %d", &cmd_sel, &arg1, &arg2, &arg3, &arg4);
 
-        pwmLeftReq = 0;
-        pwmRightReq = 0;
-    } else { 
-        // Checks the IR sensors if it has been at least IR_POLL ms since the
-        // last check
-        if (currentTime - lastIRTime > IR_POLL) {
-            lastIRTime = currentTime;
-            
-            updateDistance();
-
-            if (!checkClear()) {
-                msg.data = "blocked";
-                chatter.publish(&msg);
-
-                pwmLeftReq = 0;
-                pwmRightReq = 0;
-            }
-        }
-
-        // Publishes encoder data if it has been at least ENC_POLL ms since the
-        // last publish
-        if (currentTime - lastEncTime > ENC_POLL) {
-            lastEncTime = currentTime;
-
-            FL_Pub.publish(&FL_ticks);
-            FR_Pub.publish(&FR_ticks);
-            RR_Pub.publish(&RR_ticks);
-            RL_Pub.publish(&RL_ticks);
+        if (num_parsed > 0) {
+            run_command(cmd_sel, arg1, arg2, arg3, arg4);
         }
     }
 
-    set_pwm();
+    unsigned long currentTime = millis();
+
+    if (currentTime - lastCmdTime > COMMAND_TIMEOUT) {
+        // Stops robot if COMMAND_TIMEOUT ms have elapsed since the last command
+        set_pwm(0, 0, 0, 0);
+    } else if (currentTime - lastIRTime > IR_POLL) {
+        // Checks the IR sensors if it has been at least IR_POLL ms since the last check
+        lastIRTime = currentTime;
+
+        updateIR();
+
+        if (!checkClear()) {
+            set_pwm(0, 0, 0, 0);
+        }
+    }
 }
