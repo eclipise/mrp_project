06d3d87e45eab989208e75304cbee763ca22489f
Garrett <96957645+eclipise@users.noreply.github.com>
2023-08-03 19:33:16 -0400

    modified mrp package to use arduino_interface

--- src/jetson/dev_ws/src/mrp/launch/rsp.launch.py
+++ src/jetson/dev_ws/src/mrp/launch/rsp.launch.py
@@ -6,6 +6,7 @@ from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
+from launch.substitutions import Command
 
 import xacro
 
@@ -18,7 +19,7 @@ def generate_launch_description():
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('mrp'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
+    robot_description_config = Command(["xacro ", xacro_file, " sim_mode:= ", use_sim_time])
     
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
