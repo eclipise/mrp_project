05a6dbe8f9d17f07afd94c47fa8e2ee4888c4050
Garrett <96957645+eclipise@users.noreply.github.com>
2023-08-03 19:40:23 -0400

    removed some leftovers from Foxy

--- src/jetson/dev_ws/src/mrp/launch/launch_robot.launch.py
+++ src/jetson/dev_ws/src/mrp/launch/launch_robot.launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
 
     diff_drive_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["diff_cont"],
     )
 
@@ -49,7 +49,7 @@ def generate_launch_description():
 
     joint_broad_spawner = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joint_broad"],
     )
 
