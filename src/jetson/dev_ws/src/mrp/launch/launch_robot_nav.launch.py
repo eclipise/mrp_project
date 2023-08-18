23b00e72317a8e3318e0383fb80a6219fe282ab7
Garrett <96957645+eclipise@users.noreply.github.com>
2023-08-17 22:02:41 -0400

    added slam and nav to launch files

--- src/jetson/dev_ws/src/mrp/launch/launch_robot_nav.launch.py
+++ src/jetson/dev_ws/src/mrp/launch/launch_robot_nav.launch.py
@@ -8,6 +8,7 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, TimerAction, RegisterEventHandler
 from launch.event_handlers import OnProcessStart, OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
 
 from launch_ros.actions import Node
 
@@ -18,32 +19,29 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
+
+    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
     twist_mux_params = os.path.join(get_package_share_directory(package_name), 'config', 'twist_mux.yaml')
     
     twist_mux = Node(
         package="twist_mux",
         executable="twist_mux",
-        parameters=[twist_mux_params, {'use_sim_time': True}],
+        parameters=[twist_mux_params],
         remappings=[('/cmd_vel_out', '/diff_cont/cmd_vel_unstamped')]
     )
 
-    gazebo_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'gazebo_params.yaml')
+    controller_params_file = os.path.join(get_package_share_directory(package_name), "config", "my_controllers.yaml")
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
-             )
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{"robot_description": robot_description}, controller_params_file],
+    )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'mrp'],
-                        output='screen')
+    delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
 
     diff_drive_spawner = Node(
         package="controller_manager",
@@ -52,9 +50,9 @@ def generate_launch_description():
     )
 
     delayed_diff_drive_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=spawn_entity,
-            on_exit=[diff_drive_spawner]
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[diff_drive_spawner]
         )
     )
 
@@ -65,9 +63,9 @@ def generate_launch_description():
     )
 
     delayed_joint_broad_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=spawn_entity,
-            on_exit=[joint_broad_spawner]
+        event_handler=OnProcessStart(
+            target_action=controller_manager,
+            on_start=[joint_broad_spawner]
         )
     )
 
@@ -105,10 +103,9 @@ def generate_launch_description():
     return LaunchDescription([
         rsp,
         twist_mux,
-        gazebo,
-        spawn_entity,
+        delayed_controller_manager,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner,
-        delayed_slam,
+        delayed_slam, 
         delayed_nav2,
     ])
